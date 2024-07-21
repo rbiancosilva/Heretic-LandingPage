20fb6b0d321fcadc2ee4f163c700084a3213faec
rafinha <rbiancosilva45@gmail.com>
2024-07-21 19:58:32 -0300

    AOS added

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -533,6 +533,16 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"3cYfC":[function(require,module,exports) {
 AOS.init();
+const oscarTS = new Date("Mar 2, 2025 21:00:00").getTime();
+setInterval(function() {
+    const nowTS = new Date().getTime();
+    const timeDiff = oscarTS - nowTS;
+    const days = Math.floor(timeDiff / 86400000);
+    const hours = Math.floor(timeDiff % 86400000 / 3600000);
+    const minutes = Math.floor(timeDiff % 3600000 / 60000);
+    const seconds = Math.floor(timeDiff % 60000 / 1000);
+    document.getElementById("counter").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}, 1000);
 
 },{}]},["7age3","3cYfC"], "3cYfC", "parcelRequire50e1")
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 AOS.init();
+
+const oscarTS = (new Date("Mar 2, 2025 21:00:00")).getTime();
+
+setInterval(function() {
+    const nowTS = (new Date()).getTime();
+    const timeDiff = oscarTS - nowTS;
+
+
+    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(timeDiff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    const minutes = Math.floor(timeDiff % (1000 * 60 * 60) / (1000 * 60));
+    const seconds = Math.floor(timeDiff % (1000 * 60) / 1000);
+
+    document.getElementById('counter').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`
+
+}, 1000)
